47d1a9a887f8ff7aeec7281419f16dd995aeee8a
Charaf <charafeddine.omankhir13@hotmail.com>
2023-02-13 15:41:31 +0100

    Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ import pickle
 pipeline = joblib.load('app/pipeline-nums-col-scoring')
 pipeline_nums = joblib.load('app/pipeline-nums-col-scoring')
 
-X_train = pd.read_csv('data\X_train.csv')
-y_train = pd.read_csv('data\y_train.csv')
+X_train = pd.read_csv('data/X_train.csv')
+y_train = pd.read_csv('data/y_train.csv')
 pipeline = joblib.load('app/pipeline-nums-col-scoring')
 pipeline_nums = joblib.load('app/pipeline-nums-col-scoring')
 pipeline.fit(X_train, y_train.TARGET)
