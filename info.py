8de5683a7aacb1b663200254e2d603a60ee126fd
Charaf <charafeddine.omankhir13@hotmail.com>
2023-02-15 22:04:18 +0100

    test azure

--- info.py
+++ info.py
@@ -45,3 +45,31 @@ shapley_data = np.array(response_dict_shapley['shapley_data'])
 
 explainer = shap.Explanation(shapley_values, shapley_base_values, shapley_data)
 st.pyplot(shap.waterfall_plot(explainer[0][:, 1], show = False))
+
+
+#X_train = pd.read_csv('data/X_train.csv')
+app  = Flask(__name__)
+#test = 'https://ocrscoringapp.blob.core.windows.net/containerocr/X_train.csv?sp=r&st=2023-02-15T15:36:12Z&se=2023-02-15T23:36:12Z&spr=https&sv=2021-06-08&sr=b&sig=ESWBGCG5wLQvo1rj1sElWeKWSL6ql5fa3ScBUumrFF4%3D'
+#y_train = pd.read_csv(test)
+# x_train = pd.read_csv(test)
+# locations = x_train.loc[[0]]
+with open('app/columns_name_nums.pickle', 'rb') as f:
+    nums_columns_name = pickle.load(f)
+
+pipeline_nums = joblib.load('app/pipeline-nums-col-scoring')
+pipeline = joblib.load('app/pipeline-xgboost-scoring')
+
+#x_scaled = pipeline_nums.transform(locations[nums_columns_name])
+
+# @app.route('/')
+# def test():
+#     return {"test:":x_scaled.tolist()}
+
+# @app.route('/test2')
+# def test2():
+#     return {"test2:!!!":'Test!!'}
+
+# with open('app/columns_name_nums.pickle', 'rb') as f:
+#     nums_columns_name = pickle.load(f)
+
+# pipeline_nums.fit(X_train[nums_columns_name])
